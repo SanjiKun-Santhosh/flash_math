903805f45d9ff364e35ddf0cedb52ab767f13839
SanjiKun-Santhosh <santhoshparamasivam96@gmail.com>
2025-09-23 18:42:16 +0300

    Updated levels for each game

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+import java.io.FileInputStream
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,6 +7,11 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
 
+}
+val keystoreProperties=Properties()
+val keystorePropertiesFile=rootProject.file("key.properties")
+if(keystorePropertiesFile.exists()){
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -31,12 +38,21 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+          //  signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
@@ -46,6 +62,7 @@ flutter {
 }
 dependencies {
     // Import the Firebase BoM
+    implementation("com.google.android.material:material:1.14.0-alpha04")
     implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
 
 
